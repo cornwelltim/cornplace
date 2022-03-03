adf125d82265271540339a36d3170fffb546838b
cornwelltim <cornwellt.93@gmail.com>
2022-03-03 16:02:19 +0600

    Update featured.php

--- portfolio/featured.php
+++ portfolio/featured.php
@@ -1,3 +1,10 @@
-<iframe src="https://drive.google.com/file/d/1bZv-UX_sD_KAMyE7g6wp3mOX4xjxGnWL/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
-<iframe src="https://drive.google.com/file/d/1EOLy5lzjYo6NoJPYnjOosIllTLeLCpv7/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1nEQhIzV8i-fJHU7-0XZRrLI1BsGgcqtf/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1n-J0WemsA5PyGuziqjUsB0S0XPAEgVX0/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
 <iframe src="https://drive.google.com/file/d/17AZKYCeihuYX1tVIjlrtQWr6MI-bv3dN/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1mpzgEznww2uOQ0f6kMvG1h8lVcR1m9az/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1bZv-UX_sD_KAMyE7g6wp3mOX4xjxGnWL/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1Ayrxu13FVXtKU3uJo06JMZ9GrlcjQtUP/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1YAIqAce-oOphWGJrhrR8un8AgVAT74eG/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1hboOc0Mj82329EW0tUYua9aGqsoDoUC1/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1wMpePGPoWqS79aScBP4M8Qf0u5ofETUt/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
+<iframe src="https://drive.google.com/file/d/1tOteqFFXOXbrClWqv9ncVpgClHKDPHO4/preview" width="600" height="340" allow="autoplay" allowfullscreen="allowfullscreen"></iframe>
